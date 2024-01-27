6b67af679455a5c5d74bd19076fc067648289e65
juliannegodsey1 <juliannegodseyy@gmail.com>
2024-01-27 13:53:15 -0500

    exercise 1 redo complete

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var a = 17;
+            var b = 4;
+
+            var sum = a + b;
+            var difference = a - b;
+            var product = a * b;
+            var quotient = a / b;
+            var remainder = a % b;
+
+
+            Console.WriteLine($"The sum of {a} and {b} is {sum}");
+            Console.WriteLine($"The difference between {a} and {b} is {difference}");
+            Console.WriteLine($"The product of {a} and {b} is {product}");
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+
         }
     }
 }
